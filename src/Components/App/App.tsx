aa0a3e95a6d96b43acb228af5006339942abca6b
Kyle Williams <kylecharleswilliams@gmail.com>
2021-08-27 00:49:01 -0600

    Add title input box for to do

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,11 +1,21 @@
 import './App.css';
 import ToDos from '../ToDos/ToDos';
+import { useState, useEffect, FormEvent } from 'react';
+import CreateToDos from '../CreateToDos/CreateToDos';
 
 const App = () => {
+  const [ toDoTitle, setToDoTitle ] = useState('');
+
+  const addTitle = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const input = event.currentTarget.value;
+    setToDoTitle(input);
+  }
   return (
     <div className="App">
       <h1>My To Dos</h1>
       <ToDos />
+      <CreateToDos addTitle={addTitle} />
     </div>
   );
 }
--- src/Components/CreateToDos/CreateToDos.tsx
+++ src/Components/CreateToDos/CreateToDos.tsx
@@ -1,10 +1,17 @@
-export type Props = {
+import React, { FC } from 'react';
 
+
+type Props = {
+  addTitle: any;
 }
 
-const CreateToDos = () => {
+const CreateToDos: FC<Props> = ({ addTitle }) => {
   return (
-    <div></div>
+    <div>
+      <form>
+        <input type='text' onChange={addTitle}></input>
+      </form>
+    </div>
   )
 }
 
